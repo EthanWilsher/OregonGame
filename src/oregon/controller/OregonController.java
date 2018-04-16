f16a32790854c69a6b30915e7c61207129a3c380
ewil1026 <wilshdog@gmail.com>
2018-04-15 20:55:25 -0600

    initialized variables added some loop methods and the code for the story.

--- src/oregon/controller/OregonController.java
+++ src/oregon/controller/OregonController.java
@@ -1,12 +1,16 @@
 package oregon.controller;
+import java.util.Scanner;
 
 public class OregonController 
 {
 	private int gameStage = 0;
+	private int armySize = 15;
+	private boolean quitGame = false;
+	private Scanner scanner;
 	
 	public void start()
 	{
-		int option = 0;
+		scanner = new Scanner(System.in);
 		
 		System.out.println("You are the General of your army. Your goal is to capture the enemy base."
 				+ " You have an army of 15 men. The game is over when you either capture the enemy base or"
@@ -18,12 +22,29 @@ public class OregonController
 			
 			ProgressStory();
 			
-		}while(option != 3);
+			PrintGameStatus();
+			
+			if(armySize <= 0 )
+			{
+				System.out.println("Your army has been wiped out. You lose! Game Over!");
+				quitGame = true;
+			}
+			
+		}while(quitGame == false);
 		
+		System.out.println("Thanks for playing! Goodbye");
+		
+	}
+	
+	private void PrintGameStatus()
+	{
+		System.out.println("After Stage" + gameStage + "of the game your army's size is: " + armySize);
 	}
 	
 	private void ProgressStory()
 	{
+		int selectedOption = 0;
+		
 		switch(gameStage)
 		{
 			case 1: 
@@ -31,26 +52,157 @@ public class OregonController
 				System.out.println("1. Go across the lake?");
 				System.out.println("2. Go around the lake?");
 				System.out.println("3. Quit the game");
+				
+				selectedOption = scanner.nextInt();
+				
+				ApplyConsequencesOfSelection(selectedOption);
+				
 				break;
 			case 2:
-				// output story and options
+				System.out.println("You are marching and come across a lake. Do you:");
+				System.out.println("1. Go across the lake?");
+				System.out.println("2. Go around the lake?");
+				System.out.println("3. Quit the game");
+				
+				selectedOption = scanner.nextInt();
+				
+				ApplyConsequencesOfSelection(selectedOption);
 				break;
 			case 3: 
-				// output story and options
+				System.out.println("You are marching and come across a lake. Do you:");
+				System.out.println("1. Go across the lake?");
+				System.out.println("2. Go around the lake?");
+				System.out.println("3. Quit the game");
+				
+				selectedOption = scanner.nextInt();
+				
+				ApplyConsequencesOfSelection(selectedOption);
 				break;
 			case 4:
-				// output story and options
+				System.out.println("You are marching and come across a lake. Do you:");
+				System.out.println("1. Go across the lake?");
+				System.out.println("2. Go around the lake?");
+				System.out.println("3. Quit the game");
+				
+				selectedOption = scanner.nextInt();
+				
+				ApplyConsequencesOfSelection(selectedOption);
 				break;
 			case 5: 
-				// output story and options
+				System.out.println("You are marching and come across a lake. Do you:");
+				System.out.println("1. Go across the lake?");
+				System.out.println("2. Go around the lake?");
+				System.out.println("3. Quit the game");
+				
+				selectedOption = scanner.nextInt();
+				
+				ApplyConsequencesOfSelection(selectedOption);
 				break;
 			case 6:
-				// output story and options
+				System.out.println("You are marching and come across a lake. Do you:");
+				System.out.println("1. Go across the lake?");
+				System.out.println("2. Go around the lake?");
+				System.out.println("3. Quit the game");
+				
+				selectedOption = scanner.nextInt();
+				
+				ApplyConsequencesOfSelection(selectedOption);
 				break;
 				
 			
 		}
 	
+	}
+	
+	private void ApplyConsequencesOfSelection(int optionSelected)
+	{
+		switch(gameStage)
+		{
+		case 1: 
+			if(optionSelected == 1)
+			{
+				armySize -= 2;
+			}
+			else if(optionSelected == 2)
+			{
+				
+			}
+			else
+			{
+				quitGame = true;
+			}
+			break;
+		case 2:
+			if(optionSelected == 1)
+			{
+				
+			}
+			else if(optionSelected == 2)
+			{
+				
+			}
+			else
+			{
+				quitGame = true;
+			}
+			break;
+		case 3: 
+			if(optionSelected == 1)
+			{
+				
+			}
+			else if(optionSelected == 2)
+			{
+				
+			}
+			else
+			{
+				quitGame = true;
+			}
+			break;
+		case 4:
+			if(optionSelected == 1)
+			{
+				
+			}
+			else if(optionSelected == 2)
+			{
+				
+			}
+			else
+			{
+				quitGame = true;
+			}
+			break;
+		case 5: 
+			if(optionSelected == 1)
+			{
+				
+			}
+			else if(optionSelected == 2)
+			{
+				
+			}
+			else
+			{
+				quitGame = true;
+			}
+			break;
+		case 6:
+			if(optionSelected == 1)
+			{
+				
+			}
+			else if(optionSelected == 2)
+			{
+				
+			}
+			else
+			{
+				quitGame = true;
+			}
+			break;
+		}
 	}
 	
 	
